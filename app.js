581210a419a929bf24606121fd7f47d64b9f31e1
Dmitry <d.wygoniajlo@gmail.com>
2021-03-31 17:42:49 +0300

    added new section, added slider

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   $('.slider').slick({
+      arrows:true,
+      dots:true,
+      adaptiveHeight:true,
+      autoplay: true,
+      // slidesToShow:3,
+      // slidesToScroll:3,
+      // speed:100,
+      // easing: 'linear',
+      // infinite: true,
+      
+   });
+});
