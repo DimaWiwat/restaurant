e07add3d7791c1c03cfb19aee9b0b1b4825c9dc0
Dmitry <d.wygoniajlo@gmail.com>
2021-04-01 17:18:57 +0300

    added tabs,map,footer

--- app.js
+++ app.js
@@ -12,3 +12,40 @@ $(document).ready(function() {
       
    });
 });
+
+let tabs = document.querySelectorAll(".tab-title-item");
+let tabContent = document.querySelectorAll(".tab-content");
+let tabsWrap = document.querySelector(".tab-title-items")
+
+function hideTabContent() {
+   tabContent.forEach(tab => {
+      tab.classList.add("hide");
+      tab.classList.remove("show");
+   })
+
+   tabs.forEach(item => {
+      item.classList.remove("tab-active");
+   })
+}
+
+function showTabContent(item = 0) {
+   tabContent[item].classList.add("show");
+   tabContent[item].classList.remove("hide");
+   tabs[item].classList.add("tab-active");
+}
+
+hideTabContent()
+showTabContent()
+
+tabsWrap.addEventListener("click",function(e) {
+   let target = e.target
+
+   if(target && target.classList.contains("tab-title-item")) {
+      tabs.forEach((item,i) => {
+         if (target === item) {
+            hideTabContent();
+            showTabContent(i);
+         }
+      })
+   }
+})
