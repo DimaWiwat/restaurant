0540ccf7a425733d991d2a67d01f07f0e21a6756
Dmitry <d.wygoniajlo@gmail.com>
2021-04-07 19:49:36 +0300

    added burger menu

--- app.js
+++ app.js
@@ -100,3 +100,19 @@ modal.addEventListener('click', function(e) {
 // }
 
 // window.addEventListener("scroll",showModalByScroll)
+
+
+// mobile menu
+
+let mobileMenu = document.querySelector('.nav-mobile-menu');
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function() {
+   mobileMenu.classList.toggle("active-menu");
+   if(mobileMenu.classList.contains("active-menu")) {
+      mainMenu.classList.add("active-menu");
+   } else {
+      mainMenu.classList.remove("active-menu");
+   }  
+
+})
